1490485635261e782c33be13b2a69964733456ba
andrewletz <andrewletz@gmail.com>
2019-03-15 20:05:50 -0700

    Eight directional sprite basically complete

--- PlayerSprite.gd
+++ PlayerSprite.gd
@@ -18,10 +18,37 @@ func _process(delta):
 	
 	var mousepos = get_viewport().get_mouse_position()
 	var size = get_viewport().size
-
-	var row = 0
+	
 	set_flip(false)
-	if (mousepos.x < (size.x / 2)):
-		set_flip(true)
+	if mouse_has_control: # only move sprite if we should be
+		# get the sprite's location in device space
+		var sprite_loc = camera.unproject_position(get_global_transform().origin)
+		# get vector of relative position between sprite and mouse
+		var relativepos = Vector2(mousepos.x - sprite_loc.x, mousepos.y - sprite_loc.y)
+		var rads = atan2(relativepos.y, relativepos.x)
+		var ang = rad2deg(rads) # get angle that sprite should be facing
+		if ang < 0:
+			ang += 360
+		
+		# find sprite direction based on angle
+		if ang < 30 or ang > 330:
+			anim_row = 2 # right
+		elif ang < 60:
+			anim_row = 1 # down right
+		elif ang < 120:
+			anim_row = 0 # down
+		elif ang < 150:
+			anim_row = 1 # down left
+		elif ang < 180:
+			anim_row = 2 # left
+		elif ang < 210:
+			anim_row = 3 # up left
+		elif ang < 270:
+			anim_row = 4 # up
+		elif ang < 330:
+			anim_row = 3 # up right
+		
+		if ang > 90 and ang < 270:
+			set_flip(true)
 	
 	frame = anim_col + anim_row * 4
